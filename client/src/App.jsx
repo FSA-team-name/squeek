9c4ab1ef32762a6c99ba7d44816bbb188c1c6f3f
JT4848 <jtpricone@gmail.com>
2023-09-07 19:47:38 -0400

    fixed app.jsx and navbar.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, Link } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { MakeSqueeks, Navbar, Searchbar, Suggested, Favorites, Home, Profile, Thread, Message } from './imports';
 import Communities from './pages/Communities';
 import Explore from './pages/Explore';
@@ -7,26 +7,9 @@ const App = () => {
 
   return (
     <>
-    <div id="navbar">
-          <Link to="/" className="linkstyle">
-            Home
-          </Link>
-          <Link to="/explore" className="linkstyle">
-            Explore
-          </Link>
-          <Link to="/message" className="linkstyle">
-            Messages
-          </Link>
-          <Link to="/favorites" className="linkstyle">
-            Favorites
-          </Link>
-          <Link to="/communities" className="linkstyle">
-            Communities
-          </Link>
-          <Link to="/Profile" className="linkstyle">
-            Profile
-          </Link>
-        </div>
+
+      <Navbar />
+      
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/explore' element={<Explore />}/>
