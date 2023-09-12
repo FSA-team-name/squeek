0da06a7553baa17a2d863eba9d8ec40cd4acb307
Mateo071 <floresmateo2000@gmail.com>
2023-09-11 20:51:04 -0400

    finish navbar bug

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,56 +11,56 @@ const Navbar = () => {
       <nav className={`${open ? 'w-72' : 'w-20'} bg-toothwhite h-screen p-5 pt-8 relative duration-300`}>
           <img src={control} className={`absolute -right-3 top-9 w-7 border-earlgrey border-2 rounded-full ${!open && 'rotate-180'}`} onClick={() => setOpen(!open)}/>
           <div className='flex gap-x-4 items-center'>
-            <img src={logo} alt='Logo' className={`w-10 duration-500 ${open && 'rotate-[360]'}`}/>
+            <img src={logo} alt='Logo' className={`w-10 duration-500 ${open && 'rotate-[360deg]'}`}/>
             <h1 className={`text-earlgrey relative font-medium text-xl duration-300 ${!open && 'scale-0'}`}>Squeek </h1>
           </div>
         <ul className='pt-6'>
           <li className={`text-earlgrey flex items-center gap-x-4 p-2 hover:bg-mickeygrey rounded-md mt-2`}>
           <Link to="/">
             <i className='bi-house'></i>
-            <span className={`${!open && 'hidden'} relative duration-200 ml-2`}>Home</span>
+            <h1 className={`${!open && 'scale-0'} relative duration-300 ml-2`}>Home</h1>
           </Link>
           </li>
           <li className={`text-earlgrey flex items-center gap-x-4 p-2 hover:bg-mickeygrey rounded-md mt-2`}>
           <Link to="/explore" >
             <i className='bi-search'></i>
-            <span className={`${!open && 'hidden'} relative duration-200 ml-2`}>Explore</span>
+            <h1 className={`${!open && 'scale-0'} relative duration-300 ml-2`}>Explore</h1>
           </Link>
           </li>
           <li className={`text-earlgrey flex items-center gap-x-4 p-2 hover:bg-mickeygrey rounded-md mt-2`}>
           <Link to="/message">
-            <i className='bi-chat-dots relative w-4 h-2 text-lg align-middle'></i>
-            <span className={`${!open && 'hidden'} relative duration-200 ml-2`}>Messages</span>
+            <i className='bi-chat-dots'></i>
+            <h1 className={`${!open && 'scale-0'} relative duration-300 ml-2`}>Messages</h1>
           </Link>
           </li>
           <li className={`text-earlgrey flex items-center gap-x-4 p-2 hover:bg-mickeygrey rounded-md mt-2`}>
           <Link to="/favorites">
-            <i className='bi-bookmark relative w-4 h-2 text-lg align-middle'></i>
-            <span className={`${!open && 'hidden'} relative duration-200 ml-2`}>Favorites</span>
+            <i className='bi-bookmark'></i>
+            <h1 className={`${!open && 'scale-0'} relative duration-300 ml-2`}>Favorites</h1>
           </Link>
           </li>
           <li className={`text-earlgrey flex items-center gap-x-4 p-2 hover:bg-mickeygrey rounded-md mt-2`}>
           <Link to="/communities">
-            <i className='bi-people relative w-4 h-2 text-lg align-middle'></i>
-            <span className={`${!open && 'hidden'} relative duration-200 ml-2`}>Communities</span>
+            <i className='bi-people'></i>
+            <h1 className={`${!open && 'scale-0'} relative duration-300 ml-2`}>Communities</h1>
           </Link>
           </li>
           <li className={`text-earlgrey flex items-center gap-x-4 p-2 hover:bg-mickeygrey rounded-md mt-9`}>
             <Link to="/profile">
-              <i className='bi-person relative w-4 h-2 text-lg align-middle'></i>
-              <span className={`${!open && 'hidden'} relative duration-200 ml-2`}>Profile</span>
+              <i className='bi-person'></i>
+              <h1 className={`${!open && 'scale-0'} relative duration-300 ml-2`}>Profile</h1>
             </Link>
           </li>
           <li className={`text-earlgrey flex items-center gap-x-4 p-2 hover:bg-mickeygrey rounded-md mt-2`}>
             <Link to="/signup">
-              <i className='bi-person relative w-4 h-2 text-lg align-middle'></i>
-              <span className={`${!open && 'hidden'} relative duration-200 ml-2`}>Sign Up</span>
+              <i className='bi-person'></i>
+              <h1 className={`${!open && 'scale-0'} relative duration-300 ml-2`}>Sign Up</h1>
             </Link>
           </li>
           <li className={`text-earlgrey flex items-center gap-x-4 p-2 hover:bg-mickeygrey rounded-md mt-2`}>
             <Link to="/login">
-              <i className='bi-person relative w-4 h-2 text-lg align-middle'></i>
-              <span className={`${!open && 'hidden'} relative duration-200 ml-2`}>Log In</span>
+              <i className='bi-person'></i>
+              <h1 className={`${!open && 'scale-0'} relative duration-300 ml-2`}>Log In</h1>
             </Link>
           </li>
         </ul>
