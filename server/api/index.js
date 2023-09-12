a2e366acafa9e20cea0988c86619fd347c06775b
Wyatt Rajer <wyattrajer@gmail.com>
2023-09-12 14:13:50 -0400

    squeeks API route init

--- server/api/index.js
+++ server/api/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const search = require('./search')
+const squeeks = require('./squeeks')
 
 router.get("/", (req, res) => {
   res.send("API router working");
 });
 
 router.use('/search', search);
+router.use('/squeeks', squeeks);
 
 module.exports = router;
--- server/api/squeeks.js
+++ server/api/squeeks.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { PrismaClient } = require('@prisma/client')
-
-const prisma = new PrismaClient()
 
 router.get("/", (req, res) => {
-  res.send("Search router working");
+  res.send("Squeeks router working");
 });
 
 module.exports = router;
