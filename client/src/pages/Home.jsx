f733773453c6dcb837bd71fcf9ae762baf37a83f
Wyatt Rajer <wyattrajer@gmail.com>
2023-09-12 15:06:26 -0400

    setup effect / added tyler script

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,18 @@
+import { useState, useEffect } from "react";
+
 const Home = () => {
 
+  const [squeeks, setSqueeks] = useState([]);
+
+  useEffect(() => {
+    console.log(`hello`)
+  }, [])
+
   return (
-      <section className='p-7 text-2xl font-semibold flex-1 h-screen'>
-        <h1>Home</h1>
-      </section>
-    
-  )
-}
+    <section className="p-7 text-2xl font-semibold flex-1 h-screen">
+      <h1>Home</h1>
+    </section>
+  );
+};
 
 export default Home;
