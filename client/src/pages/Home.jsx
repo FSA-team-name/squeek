e6c00681273cafa9a8e307246f2d047876336de7
Wyatt Rajer <wyattrajer@gmail.com>
2023-09-13 11:53:29 -0400

    init styling on squeeks

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -19,16 +19,16 @@ const Home = () => {
   }, [])
 
   return (
-    <section className="p-7 text-2xl font-semibold flex-1 h-screen">
+    <section className="flex-1 h-screen">
       {squeeks.map(squeek => {
         return (
-          <section>
-            <section>
+          <section key={squeek.id} className="flex max-w-xl flex-col items-start justify-between">
+            <section className="flex items-center gap-x-4 text-xs">
               <h3>{squeek.author.username}</h3>
               <h2>{squeek.author.firstName}</h2>
-              <section>
-                <p>{squeek.text}</p>
-                <p>{squeek.dateTimeCreated}</p>
+              <section >
+                <p className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">{squeek.text}</p>
+                <p className="text-gray-500">{squeek.dateTimeCreated}</p>
               </section>
               <section>
                 {squeek.reSqueekId ? <ReSqueek squeekID={squeek.reSqueekId} /> : null}
