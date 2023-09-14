6657ba74161f45ad2c3c0707a74ace77fdfc4752
JT4848 <jtpricone@gmail.com>
2023-09-14 18:16:23 -0400

    scaffolding for favorites

--- client/src/pages/Favorites.jsx
+++ client/src/pages/Favorites.jsx
@@ -1,8 +1,90 @@
+import { useState } from "react";
+
 const Favorites = () => {
+  const [activeTab, setActiveTab] = useState("Liked"); 
+  const [title, setTitle] = useState("Liked"); 
+
+  const handleTabClick = (tab) => {
+    if (activeTab === tab) {
+      setActiveTab("Liked");
+      setTitle("Liked"); 
+    } else {
+      setActiveTab(tab);
+      setTitle(tab); 
+    }
+  };
+  const tabContent = {
+    Liked: (
+      <div>
+        <h1>liked</h1>
+        <h1>liked</h1>
+        <h1>liked</h1>
+        <h1>liked</h1>
+        <h1>liked</h1>
+        <h1>liked</h1>
+        <h1>liked</h1>
+        <h1>liked</h1>
+      </div>
+    ),
+    Saved: (
+      <div>
+        <h1>saved</h1>
+        <h1>saved</h1>
+        <h1>saved</h1>
+        <h1>saved</h1>
+      </div>
+    ),
+    Dislikes: (
+      <div>
+        <h1>dislikes</h1>
+        <h1>dislikes</h1>
+        <h1>dislikes</h1>
+        <h1>dislikes</h1>
+        <h1>dislikes</h1>
+        <h1>dislikes</h1>
+        <h1>dislikes</h1>
+        <h1>dislikes</h1>
+        <h1>dislikes</h1>
+        <h1>dislikes</h1>
+        <h1>dislikes</h1>
+        <h1>dislikes</h1>
+        <h1>dislikes</h1>
+        <h1>dislikes</h1>
+        <h1>dislikes</h1>
+        <h1>dislikes</h1>
+      </div>
+    ),
+    "My List": (
+      <div>
+        <h1>List of stuff</h1>
+        <h1>List of stuff</h1>
+      </div>
+    ),
+  };
 
   return (
-    <h1>Favorites</h1>
-  )
-}
+    <div className="container mx-auto mt-10 p-4">
+      <h1 className="text-4xl font-semibold mb-4 text-center">{title}</h1>
+      <div className="bg-white shadow-md rounded-lg border-2 border-cheeseyellow p-4">
+        <div className="flex">
+          {Object.keys(tabContent).map((tab) => (
+            <button
+              key={tab}
+              className={`flex-grow p-4 ${
+                activeTab === tab
+                  ? "shadow-lg"
+                  : ""
+              }`}
+              onClick={() => handleTabClick(tab)}
+            >
+              {tab}
+            </button>
+          ))}
+        </div>
+        <div className="p-4">{tabContent[activeTab]}</div>
+      </div>
+    </div>
+  );
+};
 
 export default Favorites;
