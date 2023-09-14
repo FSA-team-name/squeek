9eb10b8ab9aa3d92d9ea9384b61ebde80ecc35a6
Wyatt Rajer <wyattrajer@gmail.com>
2023-09-14 14:37:23 -0400

    Posts new tweet with hardcoded token

--- client/src/components/MakeSqueeks.jsx
+++ client/src/components/MakeSqueeks.jsx
@@ -4,9 +4,24 @@ const MakeSqueeks = () => {
 
   const [squeekInput, setSqueekInput] = useState('');
 
+  const postSqueek = async (input) => {
+    try {
+      const response = await fetch('/api/squeeks', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjk0NzEwNzE0fQ.ryh2oIMXmIZw9OmTeWYpriQsn-JNUar8BtKi3oxrvb8"
+        },
+        body: JSON.stringify({text: input})
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   const formHandler = (e) => {
     e.preventDefault();
-    console.log(squeekInput)
+    postSqueek(squeekInput);
   }
   
   return (
