1ad29b9c2e0346db1643d69e30d0498e7a873a58
Wyatt Rajer <wyattrajer@gmail.com>
2023-09-14 10:01:33 -0400

    moved colors inside extended:{}

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,13 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    colors: {
-      earlgrey: 'rgb(85, 83, 83)',
-      mickeygrey: 'rgb(193, 193, 193)',
-      cheeseyellow: 'rgb(253, 204, 75)',
-      toothwhite: 'rgb(250, 250, 250)',
-      backgroundgrey: 'rgb(207, 207, 207)'
+    extend: {
+      colors: {
+        earlgrey: 'rgb(85, 83, 83)',
+        mickeygrey: 'rgb(193, 193, 193)',
+        cheeseyellow: 'rgb(253, 204, 75)',
+        toothwhite: 'rgb(250, 250, 250)',
+        backgroundgrey: 'rgb(207, 207, 207)',
+        blue: 'rgb(37 99 235)'
+      }
     }
   },
   plugins: [],
