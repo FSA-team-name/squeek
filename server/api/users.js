a9295c5770c0a3b757e70bcac5e19c64e75aa85e
Wyatt Rajer <wyattrajer@gmail.com>
2023-09-15 11:14:46 -0400

    /me returns user info based on token information

--- server/api/users.js
+++ server/api/users.js
@@ -10,4 +10,26 @@ router.get('/', (req, res) => {
   res.send('users route working')
 })
 
+router.get('/me', checkAuth, async (req, res) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {id: req.user},
+      select: {
+        username: true,
+        firstName: true,
+        lastName: true,
+        photo: true,
+        dateJoined: true,
+        verified: true,
+        squeeks: true
+      }
+    });
+    res.status(200).send(user)
+  } catch (err) {
+    console.log(err);
+  }
+})
+
+
+
 module.exports = router;
