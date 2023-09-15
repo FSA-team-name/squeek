7f039088ac328fdc6ec509bdab1021cc77c46474
Wyatt Rajer <wyattrajer@gmail.com>
2023-09-15 11:23:25 -0400

    /:username returns user info based on username param

--- server/api/users.js
+++ server/api/users.js
@@ -30,6 +30,30 @@ router.get('/me', checkAuth, async (req, res) => {
   }
 })
 
+router.get('/:username', async (req, res) => {
+  const { username } = req.params
+  try {
+    const user = await prisma.user.findUnique({
+      where: {username: username},
+      select: {
+        username: true,
+        firstName: true,
+        lastName: true,
+        photo: true,
+        dateJoined: true,
+        verified: true,
+        squeeks: true
+      }
+    });
+    if (user) {
+      res.status(200).send(user)
+    } else {
+      res.status(404).send(`User not found`);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+})
 
 
 module.exports = router;
