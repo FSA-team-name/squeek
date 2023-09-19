ee523e8ad497d00dcd92d6a3cc0aafde131b5ead
Wyatt Rajer <wyattrajer@gmail.com>
2023-09-19 11:46:06 -0400

    Refactoring to reuse InputBox

--- client/src/components/MakeReply.jsx
+++ client/src/components/MakeReply.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 import { useSelector } from 'react-redux';
+import InputBox from "./reusables/InputBox";
 
 const MakeReply = ({ squeek, setSqueek, id }) => {
   const token = useSelector((state) => state.userToken.token);
   const [squeekInput, setSqueekInput] = useState("");
   const [characterCount, setCharacterCount] = useState(0);
-  const [keystroke, setKeystroke] = useState();
 
   const postReply = async (input) => {
     try {
@@ -44,43 +44,16 @@ const MakeReply = ({ squeek, setSqueek, id }) => {
     await getSqueek();
   };
 
-  const keyDownHandler = (e) => {
-    setKeystroke(e.key)
-  }
-
-  const changeHandler = async (e) => {
-     setCharacterCount(e.target.value.length)
-     if (keystroke === 'Backspace') {
-      return setSqueekInput(e.target.value)
-     }
-     if (e.target.value.length >= 140) {
-       return setSqueekInput(squeekInput);
-     }
-     setSqueekInput(e.target.value)
-  }
-
-
   return (
-    <section className="flex flex-col my-2 mx-2 bg-toothwhite shadow-md"> 
-        <form onSubmit={formHandler} className="flex relative items-center">
-          <textarea
-            className="w-full h-20 p-2 inline outline-none resize-none mx-2 bg-toothwhite rounded-s "
-            value={squeekInput}
-            rows={5}
-            onChange={changeHandler}
-            onKeyDown={keyDownHandler}
-            type="text"
-            placeholder="Reply to this squeek"
-          />
-      <section className="absolute bottom-0 right-11">
-       <p className="text-xs text-mickeygrey">{characterCount}/140</p>
-
-      </section>
-      <button 
-        className="my-.5 mx-4 rounded-md py-2 px-4 relative duration-300 ml-2 bg-cheeseyellow text-earlgrey hover:bg-earlgrey hover:text-cheeseyellow border border-transparent hover:border-cheeseyellow font-bold"
-      >Nibble</button>
-        </form>  
-    </section>
+    <InputBox
+    formHandler={formHandler}
+    squeekInput={squeekInput}
+    setSqueekInput={setSqueekInput}
+    characterCount={characterCount}
+    setCharacterCount={setCharacterCount}
+    placeholder="Nibble on this Squeek"
+    action="Nibble"
+  />
   );
 };
 
