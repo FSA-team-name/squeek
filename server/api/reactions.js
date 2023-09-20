3417a0dcd671224035da558c242919364a156746
Wyatt Rajer <wyattrajer@gmail.com>
2023-09-20 12:10:34 -0400

    sets dislike to true/false based on like

--- server/api/reactions.js
+++ server/api/reactions.js
@@ -9,4 +9,13 @@ router.get("/", (req, res) => {
   res.send("reactions router working");
 });
 
+router.post("/", checkAuth, (req, res) => {
+  const { squeekId, like } = req.body;
+  let trueOrFalse = false;
+  like ? trueOrFalse = false : trueOrFalse = true;
+  try {
+    console.log(trueOrFalse);
+  } catch (err) {}
+});
+
 module.exports = router;
