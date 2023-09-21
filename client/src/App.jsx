73a97dacc7ad4b05bc83fba3622ce57a2a6619d5
Mateo071 <floresmateo2000@gmail.com>
2023-09-21 17:10:36 -0400

    fixed conflict

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,31 @@
-import { Route, Routes } from 'react-router-dom';
-import { MakeSqueeks, Navbar, Searchbar, Suggested, Favorites, Home, Profile, Thread, Message, Signup, Communities, Explore, Login, Notfound } from './imports';
-import { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { Route, Routes } from "react-router-dom";
+import {
+  MakeSqueeks,
+  Navbar,
+  Searchbar,
+  Suggested,
+  Favorites,
+  Home,
+  Profile,
+  Thread,
+  Message,
+  Signup,
+  Communities,
+  Explore,
+  Login,
+  Notfound,
+} from "./imports";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import Modal from "./components/modals/Modal";
 import ReplyModalDisplay from "./components/modals/ReplyModalDisplay";
 import ReSqueekModalDisplay from "./components/modals/ReSqueekModalDisplay";
 import EditProfile from "./components/EditProfile";
-import io from 'socket.io-client';
-import { useDispatch } from 'react-redux';
+import io from "socket.io-client";
+import { useDispatch } from "react-redux";
 import { setToken } from "./redux/tokenSlice";
 
-const socket = io.connect('http://localhost:3002');
+const socket = io.connect("http://localhost:3002");
 
 const App = () => {
   const token = useSelector((state) => state.userToken.token);
@@ -19,26 +34,29 @@ const App = () => {
     (state) => state.modalState.reSqueekModal
   );
   const squeek = useSelector((state) => state.modalState.squeek);
-  
+
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getUser = async(token) => {
-      const response = await fetch('/api/users/me', {
-        method: 'GET',
-        headers: {
-          'Content-type': 'application/json',
-          'Authorization': `Bearer ${token}`
-        },
-      });
-      const data = await response.json();
-      dispatch(setToken({ id: data.id, username: data.username, token }));
-    }
+    const getUser = async (token) => {
+      try {
+        const response = await fetch("/api/users/me", {
+          method: "GET",
+          headers: {
+            "Content-type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const data = await response.json();
+        dispatch(setToken({ id: data.id, username: data.username, token }));
+      } catch (err) {
+        console.error(err);
+      }
+    };
 
-    if (localStorage.getItem('logintoken')) {
-      getUser(localStorage.getItem('logintoken'))
+    if (localStorage.getItem("logintoken")) {
+      getUser(localStorage.getItem("logintoken"));
     }
-
   }, []);
 
   return (
@@ -51,17 +69,17 @@ const App = () => {
       </Modal>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/explore' element={<Explore />}/>
-        <Route path='/message' element={<Message socket={socket} />}/>
-        <Route path='/favorites' element={<Favorites />}/>
-        <Route path='/communities' element={<Communities />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/message" element={<Message socket={socket} />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/communities" element={<Communities />} />
         <Route path="/edit-profile/:userId" element={<EditProfile />} />
-        <Route path='/profile' element={<Profile />}/>
-        <Route path='/404-notfound' element={<Notfound />} />
-        <Route path='/thread' element={<Thread />} />
-        <Route path='/signup' element={<Signup />}/>
-        <Route path='/login' element={<Login />}/>
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/404-notfound" element={<Notfound />} />
+        <Route path="/thread" element={<Thread />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
       <section className="w-72 block bg-toothwhite h-screen p-5 pt-8 top-0 duration-300 border-l-4 border-cheeseyellow">
         <Searchbar />
