e0a5cbea86e10c6088a84f942289c413c35a7c99
Wyatt Rajer <wyattrajer@gmail.com>
2023-09-22 16:47:29 -0400

    moves favorite button into its own component

--- client/src/components/reusables/SqueekDisplay.jsx
+++ client/src/components/reusables/SqueekDisplay.jsx
@@ -6,6 +6,7 @@ import DislikeInactive from "./DislikeInactive";
 import DislikeActive from "./DislikeActive";
 import LikeInactive from "./LikeInactive";
 import LikeActive from "./LikeActive";
+import FavoritesButton from "./FavoritesButton";
 import { useState } from "react";
 
 const SqueekDisplay = ({ squeek }) => {
@@ -42,7 +43,7 @@ const SqueekDisplay = ({ squeek }) => {
         },
       });
       const data = await response.json();
-      console.log(data)
+      console.log(data);
       return data;
     } catch (err) {
       console.log(err);
@@ -128,7 +129,7 @@ const SqueekDisplay = ({ squeek }) => {
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
-              className="fill-cheeseyellow w-6 h-6"
+              className="fill-amber-200 w-6 h-6"
             >
               <path
                 fillRule="evenodd"
@@ -154,22 +155,7 @@ const SqueekDisplay = ({ squeek }) => {
               />
             </svg>
           </section>
-          <section
-            onClick={() => createFavorite()}
-            className="flex cursor-pointer bg-earlgrey hover:bg-mickeygrey items-center justify-center rounded-md w-8 h-8 "
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              className="fill-cheeseyellow w-6 h-6"
-            >
-              <path
-                fillRule="evenodd"
-                d="M6.32 2.577a49.255 49.255 0 0111.36 0c1.497.174 2.57 1.46 2.57 2.93V21a.75.75 0 01-1.085.67L12 18.089l-7.165 3.583A.75.75 0 013.75 21V5.507c0-1.47 1.073-2.756 2.57-2.93z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </section>
+          <FavoritesButton createFavorite={createFavorite}/>
         </section>
       </section>
     </>
